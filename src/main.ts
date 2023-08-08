fc3b3cf26c4e13e8e56d43cb9d52fa31a7e875c3
Caizebin <743489214@qq.com>
2023-08-08 11:49:27 +0800

    新增http初始化

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,13 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import { createHttp } from "./shared/core/http";
+// 初始化http
+createHttp("", {
+  apiPrefix: "", //api通用前缀
+  useProxy: true, //是否启用代理
+  timeout: 30000, //超时时间
+  noToken: false, // 是否传token
+  showSpin: true, // 是否呈现loading效果
+});
 
 createApp(App).mount("#app");
